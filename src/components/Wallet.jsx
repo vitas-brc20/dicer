b2d1728870fa5d49f9b7b4abd315fdddb498dd85
vitas <86404854+vitas-brc20@users.noreply.github.com>
2025-11-12 14:56:55 +0900

    fix: Attempt to force client directive recognition in Wallet.jsx

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,4 +1,6 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+"use client";
+
+import React, { createContext, useContext, useState, useEffect } = 'react';
 import ProtonWebSDK from '@proton/web-sdk'; // Use the default import as a function
 
 const WalletContext = createContext({
